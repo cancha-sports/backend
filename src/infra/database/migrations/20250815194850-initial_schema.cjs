dabda17689bc822e358ae3978713b94053f05e43
Marcos Schlick <marcosschlick@gmail.com>
2025-08-17 18:04:22 -0300

    fix(database): add missing default value to created_at column in initial migration

--- src/infra/database/migrations/20250815194850-initial_schema.cjs
+++ src/infra/database/migrations/20250815194850-initial_schema.cjs
@@ -121,6 +121,7 @@ module.exports = {
       created_at: {
         type: Sequelize.DATE,
         allowNull: false,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
       },
       updated_at: {
         type: Sequelize.DATE,
