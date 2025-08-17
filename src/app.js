4aae12813eeb7df011201d1801a36d94880f4853
Marcos Schlick <marcosschlick@gmail.com>
2025-08-17 00:57:22 -0300

    feat: add /sports endpoint routing

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-import express from 'express';
+import express from "express";
+import { SportRoutes } from "./http/routes/SportRoutes.js";
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', async (req, res) => {
-  res.status(200).json({ ok: 'true' });
+app.get("/", async (req, res) => {
+  res.status(200).json({ ok: "true" });
 });
+
+app.use("/sports", SportRoutes);
 
 export default app;
