d52adafc3541e0a3b400f4038383d1ce7bb3fcb2
Marcos Schlick <marcosschlick@gmail.com>
2025-10-21 17:29:20 -0300

    feat: permit http request from anywhere

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:46061"],
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
